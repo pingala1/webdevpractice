3598622414eddb15ffe5999a246acc7c434e6a5f
pingala ghimire <pingalaghimire555@gmail.com>
2023-04-02 00:05:33 -0700

    signup-form

--- script.js
+++ script.js
@@ -9,21 +9,32 @@ submitButton.addEventListener("click",function(e){
     let signupForm =document.forms["signupForm"];
     let firstNameInput=signupForm.firstname;
     let lastNameInput=signupForm.lastname;//<input name="lastname">
-    
+    let EmailInput=signupForm.Email;
+    let passwordInput=signupForm.password;
+
     if(firstNameInput.value==""){
-        errors.firstName="firstname should not be empty";
+        errors.firstname="firstname should not be empty";
         
         }
         if(lastNameInput.value==""){
-            errors.lastName="lastname should not be empty";
+            errors.lastname="lastname should not be empty";
             
+        }
+        if(EmailInput.value==""){
+            errors.Email="Email should not be empty";
+        }
+        if(passwordInput.value.length<=5){
+            errors.password="Minimum 6 characters are needed";
         }
         console.log(errors);
         for(let error in errors){
-
+            let lastnameGroup=document.getElementById(`${error}Group`);
+            let lastnameMessage=document.querySelector(`#${error}Group .message`);
+            lastnameGroup.classList.add("error");
+            lastnameMessage.textContent=errors[error];
+        }
+        if(Object.keys(errors).length == 0){
+          signupForm.submit();
         }
     });
-    // let lastnameGroup=document.getElementById('lastnameGroup');
-    // let lastnameMessage=document.querySelector("#lastnameGroup .message");
-    // lastnameGroup.classList.add("error");
-    // lastnameMessage.textContent="lastname should not be empty";
+    
